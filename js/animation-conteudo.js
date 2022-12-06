8866ec44b3f1517a2c99e58094daa319addd8aba
Jonathan <jhowmarques2501@gmail.com>
2022-12-06 13:22:20 -0300

    first commit

---
+++ js/animation-conteudo.js
@@ -0,0 +1,54 @@
+// efeito máquina de escrever
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => (elemento.innerHTML += letra), 70 * i);
+  });
+}
+
+const frase = document.querySelector("p#texto-introducao");
+typeWriter(frase);
+
+// animação conteudo
+
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+animeScroll();
+
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(function () {
+      animeScroll();
+    }, 10)
+  );
+}
